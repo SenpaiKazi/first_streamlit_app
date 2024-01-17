234520808c62c4b29b9aaee1c87cb8a6d1ee4487
SenpaiKazi <56249021+SenpaiKazi@users.noreply.github.com>
2024-01-17 13:46:06 +0800

    Update streamlit_app.py
    Moved dataframe to below the create your own smoothie header

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 import pandas
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Menu')
@@ -11,3 +11,5 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🍓 Build Your Own Smoothie 🥝🍇')
+
+streamlit.dataframe(my_fruit_list)
