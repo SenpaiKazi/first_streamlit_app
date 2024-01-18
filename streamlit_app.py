eb72a23c1fbc79e2e848e1bcd901bd85035c8fb8
SenpaiKazi <56249021+SenpaiKazi@users.noreply.github.com>
2024-01-18 13:24:20 +0800

    Update streamlit_app.py
    Added snowflake function
    Testes snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,3 +33,11 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 #display fruityvice as data frame
 streamlit.dataframe(fruityvice_normalized)
+
+#snowflake section
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT USER(), CURRENT ACCOUNT(), CURRENT REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
